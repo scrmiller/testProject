8f1b4c34972a4b305122ec3b5210f06f1a6aba48
Sam Miller <sam@airpos.co.uk>
2013-07-27 15:38:34 +0100

    Added description of script to explain collatz

--- collatz.rb
+++ collatz.rb
@@ -1,3 +1,16 @@
+=begin
+Description: The collatz conjecture, simply put, is a curious mathmatical problem which states that if
+you take any natural number and applying two simple rules you will eventually reach 1.
+
+The two rules are as follows:
+
+1) If the number is even divide it by two
+2) If the number is odd multiply it by 3 and add 1.
+
+Below is a simple script that caculates this for the natural number 40 and outputs to console.	
+
+=end
+
 def collatz(input)
    puts "Beginning collatz run..."
    until input == 1
