c7c41fb6cdf334f359f69701c09ddb08d99284fd
Sam Miller <sam@airpos.co.uk>
2013-07-27 15:46:27 +0100

    Fixed bug in implementation. Now print out down to final outcome 1. Added output file showing example run

--- collatz.rb
+++ collatz.rb
@@ -13,16 +13,15 @@ Below is a simple script that caculates this for the natural number 40 and outpu
 
 def collatz(input)
    puts "Beginning collatz run..."
-   until input == 1
+   while input >= 1
    	puts input
 	  if input%2==0
 		
 		input=input/2
 	  else
-	  	
+	  	break if input == 1
 		input = input*3+1
 	  end
-	  
    end
 end
 
